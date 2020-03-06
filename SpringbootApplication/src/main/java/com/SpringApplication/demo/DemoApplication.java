5755e97f07f20bee655b3b1d1a96185974b4a22c
Mingkongchan0 <59065512+Mingkongchan0@users.noreply.github.com>
2020-03-06 16:52:04 +0800

    Bootstrapped spring cloud config to route via Eureka

--- SpringbootApplication/src/main/java/com/SpringApplication/demo/DemoApplication.java
+++ SpringbootApplication/src/main/java/com/SpringApplication/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.SpringApplication.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication{
